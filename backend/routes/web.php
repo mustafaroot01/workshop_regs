5ebe5ade2a6f70e3d5577d918965ae3daf229f87
MRootxx <dr.rootvb@gmail.com>
2026-02-23 23:35:19 +0300

    fix: sync server fixes - htaccess static files serving, optional any param in web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Catch-all route to serve the Vue SPA
-Route::get('/{any}', function () {
+Route::get('/{any?}', function () {
     $path = public_path('index.html');
     if (file_exists($path)) {
         return response(file_get_contents($path))->header('Content-Type', 'text/html; charset=utf-8');
